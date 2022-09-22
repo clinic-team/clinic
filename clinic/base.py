30795584d4ba1a4ffd89100a3664428a759460b1
DrgnFireYellow <ethan@milbert.dev>
2022-09-22 11:37:34 -0400

    Add help

--- clinic/base.py
+++ clinic/base.py
@@ -7,16 +7,16 @@ class CLI:
     def __init__(self, name: str):
         self.name = name
         self.commands = []
-    def add(self, name: str, args: list, func: Callable):
-        self.commands.append((name, args, func))
+    def add(self, name: str, args: list, func: Callable, help: str):
+        self.commands.append((name, args, func, help))
     def run(self):
-        if argv[1] == "help":
+        if argv[1] == "help" or argv[1] == "-h" or argv[1] == "--help":
             print(figlet_format(self.name))
             for command in self.commands:
                 argstring = ""
                 for arg in command[1]:
-                    argstring += arg
-                print(f"{command[0]} {argstring}")
+                    argstring += f"<{arg}> "
+                print(f"{command[0]} {argstring} - {command[3]}")
         else:
             for command in self.commands:
                 if argv[1] == command[0]:
--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 import clinic
 
-def test(name: str):
+def test(name: str, lastname: str):
     print(f"Hello {name}!")
 
 example = clinic.CLI("example")
-example.add("hello", ["name"], test)
+example.add("hello", ["name", "last_name"], test, "Says hello")
 example.run()
